ac6ffe93edc8016d616a1da1ea00c61ac5ff9bd4
Takajo-Soft33 <i15fujimura1s@tokuyama.ac.jp>
2019-04-22 15:01:43 +0900

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+  "os"
+
   "gocv.io/x/gocv"
 )
 
 func main() {
-  webcam, _ := gocv.VideoCaptureDevice(0)
+  //webcam, _ := gocv.VideoCaptureDevice(0)
+  filename := os.Args[1]
   window := gocv.NewWindow("Hello")
-  img := gocv.NewMat()
+  img := gocv.IMRead(filename, gocv.IMReadColor)
 
   for {
-    webcam.Read(&img)
+    //webcam.Read(&img)
     window.IMShow(img)
     window.WaitKey(1)
   }
