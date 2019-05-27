275301fd69c98caf2c24c45f913e37ff05864ebf
Takajo-Soft33 <i15fujimura1s@tokuyama.ac.jp>
2019-05-27 11:16:16 +0900

    Haar顔認識のテスト

--- face-detect.go
+++ face-detect.go
@@ -10,13 +10,13 @@ import (
 )
 
 func main() {
-  if len(os.Args) < 3 {
-    fmt.Printf("Usage: go run face-detect.go <image file> <classifier xml file>\n")
+  if len(os.Args) < 2 {
+    fmt.Printf("Usage: go run face-detect.go <image file>\n")
     return
   }
 
   filename := os.Args[1]
-  xmlFile := os.Args[2]
+  xmlFile := "opencv/data/haarcascades/haarcascade_frontalface_default.xml"
 
   img := gocv.IMRead(filename, gocv.IMReadColor)
 
@@ -33,12 +33,12 @@ func main() {
     return
   }
 
-  rects := classifier.DetectMultiScale(img)
+  size := img.Size()
+  rects := classifier.DetectMultiScaleWithParams(img, 1.1, 30, 0, image.Pt(10, 10), image.Pt(size[0], size[1]))
   fmt.Printf("found %d faces\n", len(rects))
 
   for _, r := range rects {
     gocv.Rectangle(&img, r, borderColor, 3)
-
     size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
     pt := image.Pt(r.Min.X + (r.Min.X/2) - (size.X/2), r.Min.Y - 2)
     gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1.2, borderColor, 2)
